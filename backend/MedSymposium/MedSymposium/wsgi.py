724b74c688557980bab8a59468d055f9959fa13c
meissa <roumeissa.tafer@univ-constantine2.dz>
2025-11-28 15:10:07 +0100

    Refactor code structure for improved readability and maintainability

--- backend/MedSymposium/MedSymposium/wsgi.py
+++ backend/MedSymposium/MedSymposium/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for event_platform project.
+WSGI config for MedSymposium project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_platform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MedSymposium.settings')
 
 application = get_wsgi_application()
