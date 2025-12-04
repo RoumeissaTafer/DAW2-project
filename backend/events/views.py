e9541b7d9f059d37fc6b339556bb326cdd8970ad
meissa <roumeissa.tafer@univ-constantine2.dz>
2025-12-04 11:43:56 +0100

    Remove unused serializers and admin registration for events

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,16 +1,1 @@
-from rest_framework import viewsets, permissions
-from .models import Event
-from .serializers import EventSerializer
-
-class EventViewSet(viewsets.ModelViewSet):
-    serializer_class = EventSerializer
-    # user must be logged in to access these views
-    permission_classes = [permissions.IsAuthenticated]  
-
-    def get_queryset(self):
-        # Return only events created by current user
-        return Event.objects.filter(admin=self.request.user)
-
-    def perform_create(self, serializer):
-        # Automatically set admin = current user
-        serializer.save(admin=self.request.user)
+from django.shortcuts import render
