46b498a71c883897c3ba41403316f735270ba8e9
RoumeissaTafer <roumeissa.tafer@univ-constantine2.dz>
2025-12-21 22:57:46 +0100

    create frontend folder

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -8,7 +8,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
 User = get_user_model()
 
 #SimpleJWT customization for sign in response
